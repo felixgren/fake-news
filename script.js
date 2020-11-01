6b0c42aff8f9e4179653717dbd5a5515e922e9bd
felixgren <felixg.contact@gmail.com>
2020-11-01 03:29:16 +0100

    Deleted old files

--- script.js
+++
@@ -1,9 +0,0 @@
-let burger = document.querySelector(".burger");
-let overlayMobile  = document.querySelector(".overlay-mobile");
-let navbarMobileHide = document.querySelector(".navbar-mobile");
-
-burger.addEventListener("click", function(){
-    burger.classList.toggle("burger-active")
-    overlayMobile.classList.toggle("overlay-mobile-active")
-    navbarMobileHide.classList.toggle("navbar-mobile-hide")
-})
--- variables.php
+++
@@ -1,9 +0,0 @@
-<?php
-$image = $article['image'];
-$imageAlt = $article['image_alt'];
-$title = $article['title'];
-$date = strtoupper(dateShortFormat($article['published_date']));
-$content = $article['content'];
-$author = getName($article['author_id'], $authors);
-$likes = $article['likes'];
-$link = strtolower(str_replace(" ", "-", $title));
